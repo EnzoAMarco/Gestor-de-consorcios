e22db2bdddb5bfc6a4a59a81c65cb4692c5a0712
braguilera <braaguileraa@gmail.com>
2024-09-16 22:25:36 -0300

    Clase Reclamo arreglada y funcionando

--- src/main/java/com/gestor/API/models/Reclamo.java
+++ src/main/java/com/gestor/API/models/Reclamo.java
@@ -2,7 +2,6 @@ package com.gestor.API.models;
 
 import com.gestor.API.DTOs.Estado;
 import jakarta.persistence.*;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,25 +11,29 @@ import java.util.List;
 public class Reclamo {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int idReclamo;
+	private int numero;
 
 	@ManyToOne
-	@JoinColumn(name = "documento", nullable = false)  // Especificamos @JoinColumn
-	private Persona persona;
+	@JoinColumn(name = "documento")  // Especificamos @JoinColumn
+	private Persona usuario;
 
 	@ManyToOne
-	@JoinColumn(name = "codigo", nullable = false)  // Especificamos @JoinColumn
+	@JoinColumn(name = "codigo")  // Especificamos @JoinColumn
 	private Edificio edificio;
 
-	@Column(length = 300)
+	@Column(name = "ubicacion")
 	private String ubicacion;  // Si es un área común
 
 	@ManyToOne
-	@JoinColumn(name = "identificador", nullable = true)  // Aquí también usamos @JoinColumn
+	@JoinColumn(name = "identificador")  // Aquí también usamos @JoinColumn
 	private Unidad unidad;  // Si es una unidad
 
-	@Column(nullable = false, length = 1000)
+	@Column(name = "descripcion")
 	private String descripcion;
+
+	private Estado estado;
+
+	//private List<Imagen> imagenes;
 
 	public Reclamo() {
 	}
@@ -40,7 +43,7 @@ public class Reclamo {
 		this.ubicacion = ubicacion;
 		this.descripcion = descripcion;
 		this.unidad = unidad;
-		//this.estado = Estado.nuevo;
+		this.estado = Estado.nuevo;
 		//imagenes = new ArrayList<Imagen>();
 	}
 	/*
@@ -67,15 +70,15 @@ public class Reclamo {
 		return unidad;
 	}
 
-	//public Estado getEstado() {
-		//return estado;}
+	public Estado getEstado() {
+		return estado;}
 	/*
 	public List<Imagen> getImagenes(){
 		return this.imagenes;
 	}
 	*/
-	//public void cambiarEstado(Estado estado) {
-		//this.estado = estado;}
+	public void cambiarEstado(Estado estado) {
+		this.estado = estado;}
 
 	public void save() {
 		
